9774fa17f67953c96b275e86e5e643948881580d
muyed moktadir <muyed004@gmail.com>
2022-05-07 18:58:52 +0600

    add banner

---
+++ src/pages/Home/Banner/Banner.js
@@ -0,0 +1,20 @@
+import React from "react";
+import banner from "../../../images/banner.jpg";
+import "./Banner.css"
+const Banner = () => {
+  return (
+    <div className="banner">
+      <div>
+        <h1>Bike<span style={{color: "tomato"}}>vio</span></h1>
+        <p>
+          "Travel doesn’t become adventure until you leave yourself behind".
+        </p>
+      </div>
+      <div>
+        <img src={banner} alt="" />
+      </div>
+    </div>
+  );
+};
+
+export default Banner;
---
+++ src/pages/Shared/Header/Header.js
@@ -0,0 +1,18 @@
+import React from "react";
+import CustomLink from "../../CustomLink/CustomLink";
+import './Header.css'
+
+const Header = () => {
+  return (
+    <div>
+      <nav>
+        <CustomLink to="/">Home</CustomLink>
+        <CustomLink to="/inventory">Items</CustomLink>
+        <CustomLink to="/blog">Blog</CustomLink>
+        <CustomLink to="/about">About</CustomLink>
+      </nav>
+    </div>
+  );
+};
+
+export default Header;
