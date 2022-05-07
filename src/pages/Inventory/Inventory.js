dc2e6f5a75709692f482096f59b4dec4d96279f8
muyed moktadir <muyed004@gmail.com>
2022-05-08 00:17:50 +0600

    showing a single user selected

--- src/pages/Inventory/Inventory.js
+++ src/pages/Inventory/Inventory.js
@@ -1,6 +1,9 @@
 import React from 'react';
+import { useParams } from 'react-router-dom';
 
 const Inventory = () => {
+    const {id}=useParams();
+    console.log("object",id);
     return (
         <div>
             
