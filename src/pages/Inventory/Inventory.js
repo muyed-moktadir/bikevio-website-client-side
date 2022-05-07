cb73d514344d8ca799af0b80347af39c953941a0
muyed moktadir <muyed004@gmail.com>
2022-05-08 01:19:35 +0600

    showing bike's details

--- src/pages/Inventory/Inventory.js
+++ src/pages/Inventory/Inventory.js
@@ -1,13 +1,33 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 
 const Inventory = () => {
     const {id}=useParams();
     console.log("object",id);
+    const [bike,setBike] = useState({});
+    useEffect(()=>{
+        const url = `http://localhost:5000/inventory/${id}`;
+        fetch(url)
+        .then(res=>res.json())
+        .then(data=>setBike(data))
+    },[id])
     return (
-        <div>
-            
+        <div className='bike'>
+        <div className='image'>
+            <img src={bike.img} alt="" />
         </div>
+        <div  className='bike-details'>
+            <h2>{bike.name}</h2>
+            <div style={{textAlign:'justify'}}>
+            <p title={bike.description}><strong>Description</strong>: {bike.description > 10? bike.description.slice(0,100) +'...': bike.description}</p>
+            <p><strong>supplier name</strong>: <span style={{color:'darkviolet'}}>{bike.supplier_name}</span></p>
+            <p><strong>quantity</strong>: {bike.quantity}</p>
+            <h3>price: ${bike.price}</h3>
+            </div>
+        </div>
+        <div className='btn-container'>
+        </div>
+    </div>
     );
 };
 
