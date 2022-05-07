b402cc0f8aa844d8b56fcdec1bbe2f86ab0ea5aa
muyed moktadir <muyed004@gmail.com>
2022-05-08 03:14:16 +0600

    quantity add

--- src/pages/Inventory/Inventory.js
+++ src/pages/Inventory/Inventory.js
@@ -6,38 +6,63 @@ const Inventory = () => {
   const { id } = useParams();
   console.log("object", id);
   const [bike, setBike] = useState({});
+  const {_id,name,quantity,description,supplier_name,price} = bike;
+  console.log("object",bike);
   useEffect(() => {
     const url = `http://localhost:5000/inventory/${id}`;
     fetch(url)
       .then((res) => res.json())
       .then((data) => setBike(data));
   }, [id]);
+  const handleDeliver =id=>{
+      console.log("object",id);
+      const newQuantity = quantity-1;
+    const updateQuantity ={newQuantity};
+    
+    // // TODO:send the particular user "PUT" method to server
+    // const url = `http://localhost:5000/inventory/${id}`;
+    // fetch(url,{
+    //     method:'PUT',
+    //     headers:{
+    //         'Content-Type':'application/json'
+    //     },
+    //     body: JSON.stringify(updateQuantity),
+    // })
+    // .then(res=>res.json())
+    // .then(data=>{
+    //     console.log(" success",data
+    //     );
+    //     alert('user added successfully  ..!!!')
+    //     // empty form field:
+    // })
+
+}
   return (
     <div className="bike-description">
       <div className="image">
         <img src={bike.img} alt="" />
       </div>
       <div className="bike">
-        <h2>{bike.name}</h2>
+        <h2>{name}</h2>
         <div style={{ textAlign: "justify" , paddingRight:'10px'}}>
-          <p title={bike.description}>
+          <p title={description}>
             <strong>Description</strong>:{" "}
-            {bike.description > 10
-              ? bike.description.slice(0, 100) + "..."
-              : bike.description}
+            {description > 10
+              ? description.slice(0, 100) + "..."
+              : description}
           </p>
           <p>
             <strong>supplier name</strong>:{" "}
-            <span style={{ color: "darkviolet" }}>{bike.supplier_name}</span>
+            <span style={{ color: "darkviolet" }}>{supplier_name}</span>
           </p>
           <p>
-            <strong>quantity</strong>: {bike.quantity}
+            <strong>quantity</strong>: {quantity}
           </p>
-          <h3>price: ${bike.price}</h3>
+          <h3>price: ${price}</h3>
         </div>
         <div style={{display:'flex', justifyContent:"space-evenly", marginBottom:"20px"}}>
           <div>
-            <button className="btn-class"><p >Delivered</p></button>
+            <button onClick={()=>handleDeliver(_id)} className="btn-class"><p >Delivered</p></button>
           </div>
           <div>
               <input style={{width:'60px',height:"30px"}} type="number" min="0" name=""/>{" "}
