7de92edb1dd62473e5cd77400f01bab768ad7469
muyed moktadir <muyed004@gmail.com>
2022-05-08 08:00:46 +0600

    add restock button

--- src/pages/Inventory/Inventory.js
+++ src/pages/Inventory/Inventory.js
@@ -4,52 +4,63 @@ import "./Inventory.css";
 
 const Inventory = () => {
   const { id } = useParams();
-  console.log("object", id);
-  const [bike, setBike] = useState({});
-  const {_id,name,quantity,description,supplier_name,price} = bike;
-  console.log("object",bike);
+  //   console.log("object", id);
+  const [bike,setBike] = useState([]);
+  const { _id, name, quantity, img, description, supplier_name, price } = bike;
+  console.log("single bike :", bike);
+
   useEffect(() => {
     const url = `http://localhost:5000/inventory/${id}`;
     fetch(url)
       .then((res) => res.json())
       .then((data) => setBike(data));
-  }, [id]);
-  const handleDeliver =id=>{
-      console.log("object",id);
-      const newQuantity = quantity-1;
-    const updateQuantity ={newQuantity};
-    
-    // // TODO:send the particular user "PUT" method to server
-    // const url = `http://localhost:5000/inventory/${id}`;
-    // fetch(url,{
-    //     method:'PUT',
-    //     headers:{
-    //         'Content-Type':'application/json'
-    //     },
-    //     body: JSON.stringify(updateQuantity),
-    // })
-    // .then(res=>res.json())
-    // .then(data=>{
-    //     console.log(" success",data
-    //     );
-    //     alert('user added successfully  ..!!!')
-    //     // empty form field:
-    // })
+  }, [quantity]);
 
-}
+
+  const handleDeliver = id => {
+    //   event.preventDefault()
+    console.log("object", id);
+    const newQuantity = quantity - 1;
+    console.log("new quantity", newQuantity);
+    const updateQuantity = { newQuantity };
+
+
+    // TODO:send the particular quantity "PUT" method to server
+    const url = `http://localhost:5000/inventory/${id}`;
+    fetch(url,{
+        method:'PUT',
+        headers:{
+            'content-type':'application/json'
+        },
+        body: JSON.stringify(updateQuantity),
+    })
+    .then(res=>res.json())
+    .then(data=>{
+        console.log(data)
+        setBike({ _id, name, newQuantity, img, description, supplier_name, price });
+        // window.location.reload(false);
+         alert('user added successfully  ..!!!')
+    })
+  };
+
+//   TODO:Restock 
+const handleRestock =(event)=>{
+    const quantity = event.target.value;
+    // const newQuantity = parseInt()
+    console.log("quantity",quantity);
+}   
+
   return (
     <div className="bike-description">
       <div className="image">
-        <img src={bike.img} alt="" />
+        <img src={img} alt="" />
       </div>
       <div className="bike">
         <h2>{name}</h2>
-        <div style={{ textAlign: "justify" , paddingRight:'10px'}}>
+        <div style={{ textAlign: "justify", paddingRight: "10px" }}>
           <p title={description}>
             <strong>Description</strong>:{" "}
-            {description > 10
-              ? description.slice(0, 100) + "..."
-              : description}
+            {description > 10 ? description.slice(0, 100) + "..." : description}
           </p>
           <p>
             <strong>supplier name</strong>:{" "}
@@ -60,13 +71,28 @@ const Inventory = () => {
           </p>
           <h3>price: ${price}</h3>
         </div>
-        <div style={{display:'flex', justifyContent:"space-evenly", marginBottom:"20px"}}>
+        <div
+          style={{
+            display: "flex",
+            justifyContent: "space-evenly",
+            marginBottom: "20px",
+          }}
+        >
           <div>
-            <button onClick={()=>handleDeliver(_id)} className="btn-class"><p >Delivered</p></button>
+            <button onClick={()=>handleDeliver(_id)} className="btn-class">
+              <p>Delivered</p>
+            </button>
           </div>
           <div>
-              <input style={{width:'60px',height:"30px"}} type="number" min="0" name=""/>{" "}
-            <button className="btn-class"><p>restock</p></button>
+            <input 
+              style={{ width: "60px", height: "30px" }}
+              type="number"
+              min="0"
+              name=""
+            />{" "}
+            <button onClick={handleRestock} className="btn-class">
+              <p>restock</p>
+            </button>
           </div>
         </div>
       </div>
